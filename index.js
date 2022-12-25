ac557bf85ae0ff6096c82b8f57548761b1a30714
Nik-09 <nikhil.agarwal.2019@gmail.com>
2022-12-26 02:08:34 +0530

    Adds js

--- index.js
+++ index.js
@@ -1,4 +1,55 @@
-console.log('hello')
+studName = document.getElementById('studName');
+studRoll = document.getElementById('studRoll');
+let resultId = document.getElementById('result');
+let errorMsg = document.getElementById('errorMsg')
+let history = document.getElementById('history');
+let geography = document.getElementById('geography');
+let lifeScience = document.getElementById('lifeScience');
+let physicalScience = document.getElementById('physicalScience');
+let hindi = document.getElementById('hindi');
+let english = document.getElementById('english');
+let math = document.getElementById('math');
+
+form = document.getElementById('studentNameAndRoll');
+
+form.addEventListener('submit', (e) => {
+
+    studentName = studName.value;
+    studentRoll = studRoll.value;
+    let dataFound = false;
+
+    for (let result of resultsData) {
+        if (
+            studentName == result.name &&
+            studentRoll == result.roll
+        ) {
+            // Updating marks.
+            history.innerHTML = result.history;
+            geography.innerHTML = result.geography;
+            lifeScience.innerHTML = result.lifeScience;
+            physicalScience.innerHTML = result.physicalScience;
+            hindi.innerHTML = result.hindi;
+            english.innerHTML = result.english;
+            math.innerHTML = result.math;
+
+            dataFound = true
+            resultId.style.display = "block";
+            errorMsg.style.display = "none"
+        }
+
+        if(dataFound) {
+            break;
+        }
+    }
+
+    if (!dataFound) {
+        errorMsg.style.display = "block";
+        resultId.style.display = "none"
+    }
+    e.preventDefault();
+
+});
+
 function getData() {
     console.log('Getting data..')
     fetch("result.json")
@@ -7,3 +58,28 @@ function getData() {
         console.log(data);
     })
 }
+
+let resultsData = [
+    {
+        "name": "Nikhil",
+        "roll": "120",
+        "history": "130",
+        "geography": "100",
+        "lifeScience": "20",
+        "physicalScience": "30",
+        "hindi": "30",
+        "english": "40",
+        "math": "100"
+    },
+    {
+        "name": "Rohit",
+        "roll": "130",
+        "history": "130",
+        "geography": "100",
+        "lifeScience": "20",
+        "physicalScience": "30",
+        "hindi": "30",
+        "english": "40",
+        "math": "100"
+    }
+];
